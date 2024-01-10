66a35a3b201894e5dba9fc3423f6168b90e307ff
asc33nzio <asc33nzio.dev@gmail.com>
2024-01-10 11:52:44 +0700

    feat: Added index.ts

---
+++ index.ts
@@ -0,0 +1,20 @@
+enum LogEnum {
+	"info" = "\x1b[32m",
+	"error" = "\x1b[31m",
+}
+
+class Logger {
+	private static logger: Logger | null = null;
+	private reset = "\x1b[0m";
+
+	private constructor() {}
+
+	static getInstance() {
+		if (!Logger.logger) Logger.logger = new Logger();
+		return Logger.logger;
+	}
+
+	log(logType: LogEnum, messages: string) {
+		console.log(logType + messages + this.reset);
+	}
+}
